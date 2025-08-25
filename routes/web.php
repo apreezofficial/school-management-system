38b21afbf50302d99343765d760d32791ece8de5
apreezofficial <apreezofficial@gmail.com>
2025-08-26 00:52:59 +0100

    created students migrations tables.and updated file structure

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebController;
 //Web Pages Route
-Route::Controller(WebController::class)->function(){
-  
-}
+Route::controller(WebController::class)->group(function(){
+  Route::get('/', 'index')->name('home');
+  Route::get('/', 'index')->name('home');
+ // Route::post('/school-stats', 'stats')->name('home.stats');
+});
