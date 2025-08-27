c46ae2e6db2e31509b75b243482bf9fd79a833fe
apreezofficial <apreezofficial@gmail.com>
2025-08-27 13:59:38 +0100

    removed all migrations and models related to cinema

---
+++ resources/views/404.blade.php
@@ -0,0 +1,6 @@
+<html>
+  <script src="https://cdn.tailwind.com"></script>
+  <main class="bg-red">
+    404 | Page Not Found
+  </main>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,20 @@ Route::controller(WebController::class)->group(function(){
   Route::get('/', 'index')->name('home');
  // Route::post('/school-stats', 'stats')->name('home.stats');
 });
-Route::get('/test', function(){
+Route::get('/about', function(){
+  $name = "Presh";
+  $email = "tester@gmail.com";
+//  return view('about')->with('school_name', $name)->with('contact_email', $email);
+//  return view('about', compact('name', 'email'));
+  return view('about', ['name' => $name, 'email' => $email]);
+});
+Route::view('/test', 'about', ['name' => 'Presh', 'email' => 'tester@gmail.com']);
+/**
+ * 
+ * Route::get('/test', function(){
   return 'Testing Route cache';
 });
-Route::prefix('details')->group(function(){
+ Route::prefix('details')->group(function(){
   Route::get('/teachers', function(){
   return 'Testing Route cache for teachers';
 })->name('details.teachers');
@@ -18,6 +28,7 @@ Route::get('/students/{id}', function($id){
   return 'Testing Route cache for students with id' . $id . 'Tada';
 })->name('details.student');
 });
+*/
 Route::fallback(function(){
-  return "404 | Page Not Found";
+  return view('404');
 });
