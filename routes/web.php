87905434d4810af058a99343398a6cca99db50b6
apreezofficial <apreezofficial@gmail.com>
2025-08-27 12:48:45 +0100

    Added test routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,19 @@ use App\Http\Controllers\WebController;
 //Web Pages Route
 Route::controller(WebController::class)->group(function(){
   Route::get('/', 'index')->name('home');
-  Route::get('/', 'index')->name('home');
  // Route::post('/school-stats', 'stats')->name('home.stats');
+});
+Route::get('/test', function(){
+  return 'Testing Route cache';
+});
+Route::prefix('details')->group(function(){
+  Route::get('/teachers', function(){
+  return 'Testing Route cache for teachers';
+})->name('details.teachers');
+Route::get('/students/{id}', function($id){
+  return 'Testing Route cache for students with id' . $id . 'Tada';
+})->name('details.student');
+});
+Route::fallback(function(){
+  return "404 | Page Not Found";
 });
